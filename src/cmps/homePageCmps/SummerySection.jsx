c35105c34bb9e3d8a0fd1737f30cadca553e0756
DimRev <dimarev444@gmail.com>
2024-01-22 13:09:21 +0200

    Setup draft style summery

--- src/cmps/homePageCmps/SummerySection.jsx
+++ src/cmps/homePageCmps/SummerySection.jsx
@@ -1,16 +1,20 @@
+import { SummeryCardPreview } from './SummeryCardPreview'
 export function SummerySection({}) {
   return (
-    <section className="summery">
-      <h2>Build & manage distribution team like no one else</h2>
-      <article className="summery-card">
-        <img src="" alt="summery card img" />
-        <h3>Experienced Individuals</h3>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus
-          architecto officia minima in ex qui hic enim porro consectetur
-          doloribus!
-        </p>
-      </article>
+    <section className="summery full">
+      <h2>Build & manage distribution team like no one else.</h2>
+      <SummeryCardPreview title={'Experienced Individuals'} imgUrl={'/src/assets/imgs/icon-person.svg'}>
+        Our network is made up of highly experienced professionals who are
+        passionate about what they do.
+      </SummeryCardPreview>
+      <SummeryCardPreview title={'Experienced Individuals'} imgUrl={'/src/assets/imgs/icon-cog.svg'}>
+        Our network is made up of highly experienced professionals who are
+        passionate about what they do.
+      </SummeryCardPreview>
+      <SummeryCardPreview title={'Experienced Individuals'} imgUrl={'/src/assets/imgs/icon-chart.svg'}>
+        Our network is made up of highly experienced professionals who are
+        passionate about what they do.
+      </SummeryCardPreview>
     </section>
   )
 }
