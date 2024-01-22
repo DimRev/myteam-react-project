cc120ac655057a605da73f8a2833080d63765e75
DimRev <dimarev444@gmail.com>
2024-01-22 15:05:30 +0200

    Layout fixed for Hero and Summery

--- src/cmps/homePageCmps/SummerySection.jsx
+++ src/cmps/homePageCmps/SummerySection.jsx
@@ -3,18 +3,20 @@ export function SummerySection({}) {
   return (
     <section className="summery full">
       <h2>Build & manage distribution team like no one else.</h2>
-      <SummeryCardPreview title={'Experienced Individuals'} imgUrl={'/src/assets/imgs/icon-person.svg'}>
-        Our network is made up of highly experienced professionals who are
-        passionate about what they do.
-      </SummeryCardPreview>
-      <SummeryCardPreview title={'Experienced Individuals'} imgUrl={'/src/assets/imgs/icon-cog.svg'}>
-        Our network is made up of highly experienced professionals who are
-        passionate about what they do.
-      </SummeryCardPreview>
-      <SummeryCardPreview title={'Experienced Individuals'} imgUrl={'/src/assets/imgs/icon-chart.svg'}>
-        Our network is made up of highly experienced professionals who are
-        passionate about what they do.
-      </SummeryCardPreview>
+      <section className="summery-list">
+        <SummeryCardPreview title={'Experienced Individuals'} imgUrl={'/src/assets/imgs/icon-person.svg'}>
+          Our network is made up of highly experienced professionals who are
+          passionate about what they do.
+        </SummeryCardPreview>
+        <SummeryCardPreview title={'Experienced Individuals'} imgUrl={'/src/assets/imgs/icon-cog.svg'}>
+          Our network is made up of highly experienced professionals who are
+          passionate about what they do.
+        </SummeryCardPreview>
+        <SummeryCardPreview title={'Experienced Individuals'} imgUrl={'/src/assets/imgs/icon-chart.svg'}>
+          Our network is made up of highly experienced professionals who are
+          passionate about what they do.
+        </SummeryCardPreview>
+      </section>
     </section>
   )
 }
