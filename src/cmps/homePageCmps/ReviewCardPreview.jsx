bfd6f82f9d7d787ec403813acd4f2062db7788db
DimRev <dimarev444@gmail.com>
2024-01-22 13:30:43 +0200

    Draft style reviews section

---
+++ src/cmps/homePageCmps/ReviewCardPreview.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export function ReviewCardPreview({ author, avatarUrl, children }) {
+  return (
+    <article className="reviews-card">
+      <p className="body-2">“{children}”</p>
+      <h3 className='color-teal-txt'>{author}</h3>
+      <img src={avatarUrl} alt="review-avatar" />
+    </article>
+  )
+}
--- src/cmps/homePageCmps/ReviewsSection.jsx
+++ src/cmps/homePageCmps/ReviewsSection.jsx
@@ -1,19 +1,26 @@
+import { ReviewCardPreview } from './ReviewCardPreview'
 export function ReviewsSection({}) {
   return (
-    <section className="reviews">
+    <section className="reviews full">
       <h2>
         Delivering real results for top companies. Some of our{' '}
         <span className="color-teal-txt">success stories</span>
       </h2>
-      <article className="reviews-card">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
-          consectetur laboriosam laudantium nobis perspiciatis eum, eveniet
-          provident harum. Aspernatur, accusamus.
-        </p>
-        <h3>Kady Baker</h3>
-        <img src="" alt="review-avatar" />
-      </article>
+      <ReviewCardPreview author={'Kady Baker'} avatarUrl={'/src/assets/imgs/avatar-kady.jpg'}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+        consectetur laboriosam laudantium nobis perspiciatis eum, eveniet
+        provident harum. Aspernatur, accusamus.
+      </ReviewCardPreview>
+      <ReviewCardPreview author={'Aiysha Reese'} avatarUrl={'/src/assets/imgs/avatar-aiysha.jpg'}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+        consectetur laboriosam laudantium nobis perspiciatis eum, eveniet
+        provident harum. Aspernatur, accusamus.
+      </ReviewCardPreview>
+      <ReviewCardPreview author={'Arthur Clarke'} avatarUrl={'/src/assets/imgs/avatar-arthur.jpg'}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+        consectetur laboriosam laudantium nobis perspiciatis eum, eveniet
+        provident harum. Aspernatur, accusamus.
+      </ReviewCardPreview>
     </section>
   )
 }
