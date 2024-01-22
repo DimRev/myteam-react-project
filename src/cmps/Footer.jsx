fc279bcf97bc12af6773d962bf99d4b4fabe1f7f
DimRev <dimarev444@gmail.com>
2024-01-22 16:17:57 +0200

    Add hover eff to btns

--- src/cmps/Footer.jsx
+++ src/cmps/Footer.jsx
@@ -19,9 +19,9 @@ export function Footer({}) {
         <small className="body-1">Call Us: 949-833-7432</small>
       </section>
       <section className="social-media">
-        <img src="/src/assets/imgs/icon-facebook.svg" alt="facebook" />
-        <img src="/src/assets/imgs/icon-pinterest.svg" alt="pinterest" />
-        <img src="/src/assets/imgs/icon-twitter.svg" alt="twitter" />
+        <a href=""><img src="/src/assets/imgs/icon-facebook.svg" alt="facebook" /></a>
+        <a href=""><img src="/src/assets/imgs/icon-pinterest.svg" alt="pinterest" /></a>
+        <a href=""><img src="/src/assets/imgs/icon-twitter.svg" alt="twitter" /></a>
       </section>
       <section className="copyrights">
         <small className="body-1">Copyrights 2020. All Rights Reserved</small>
