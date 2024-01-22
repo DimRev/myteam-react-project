b32ee5ee2a7e72bbb24e894a2a0a98ec1cede090
DimRev <dimarev444@gmail.com>
2024-01-22 14:22:37 +0200

    responsive header

--- src/cmps/Header.jsx
+++ src/cmps/Header.jsx
@@ -2,13 +2,18 @@ export function Header({}) {
   return (
     <header className="app-header main-layout full">
       <h2>myteam</h2>
-      <button>
+      <nav>
+        <a className="body-1" href="">home</a>
+        <a className="body-1" href="">about</a>
+      </nav>
+      <button className="menu-btn">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17">
           <g fill="#FFF" fill-rule="evenodd">
             <path d="M0 0h20v3H0zM0 7h20v3H0zM0 14h20v3H0z" />
           </g>
         </svg>
       </button>
+      <button className="contact-btn btn-outline-md">contact us</button>
     </header>
   )
 }
