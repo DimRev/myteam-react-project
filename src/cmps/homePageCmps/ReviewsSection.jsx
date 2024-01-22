3ef7ece92bf1507831eab37e095cae27932f5732
DimRev <dimarev444@gmail.com>
2024-01-22 15:19:32 +0200

    responsive reviews

--- src/cmps/homePageCmps/ReviewsSection.jsx
+++ src/cmps/homePageCmps/ReviewsSection.jsx
@@ -6,21 +6,23 @@ export function ReviewsSection({}) {
         Delivering real results for top companies. Some of our{' '}
         <span className="color-teal-txt">success stories</span>
       </h2>
-      <ReviewCardPreview author={'Kady Baker'} avatarUrl={'/src/assets/imgs/avatar-kady.jpg'}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
-        consectetur laboriosam laudantium nobis perspiciatis eum, eveniet
-        provident harum. Aspernatur, accusamus.
-      </ReviewCardPreview>
-      <ReviewCardPreview author={'Aiysha Reese'} avatarUrl={'/src/assets/imgs/avatar-aiysha.jpg'}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
-        consectetur laboriosam laudantium nobis perspiciatis eum, eveniet
-        provident harum. Aspernatur, accusamus.
-      </ReviewCardPreview>
-      <ReviewCardPreview author={'Arthur Clarke'} avatarUrl={'/src/assets/imgs/avatar-arthur.jpg'}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
-        consectetur laboriosam laudantium nobis perspiciatis eum, eveniet
-        provident harum. Aspernatur, accusamus.
-      </ReviewCardPreview>
+      <section className="review-list">
+        <ReviewCardPreview author={'Kady Baker'} avatarUrl={'/src/assets/imgs/avatar-kady.jpg'}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+          consectetur laboriosam laudantium nobis perspiciatis eum, eveniet
+          provident harum. Aspernatur, accusamus.
+        </ReviewCardPreview>
+        <ReviewCardPreview author={'Aiysha Reese'} avatarUrl={'/src/assets/imgs/avatar-aiysha.jpg'}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+          consectetur laboriosam laudantium nobis perspiciatis eum, eveniet
+          provident harum. Aspernatur, accusamus.
+        </ReviewCardPreview>
+        <ReviewCardPreview author={'Arthur Clarke'} avatarUrl={'/src/assets/imgs/avatar-arthur.jpg'}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+          consectetur laboriosam laudantium nobis perspiciatis eum, eveniet
+          provident harum. Aspernatur, accusamus.
+        </ReviewCardPreview>
+      </section>
     </section>
   )
 }
