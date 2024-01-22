952ddbec8d47cb8563a8bd9bd8e9aa88d177fbb8
DimRev <dimarev444@gmail.com>
2024-01-22 12:29:52 +0200

    Add main-layout to app

--- src/cmps/Header.jsx
+++ src/cmps/Header.jsx
@@ -1,6 +1,6 @@
 export function Header({}) {
   return (
-    <header className="app-header">
+    <header className="app-header main-layout full">
       <h2>myteam</h2>
       <button>
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17">
